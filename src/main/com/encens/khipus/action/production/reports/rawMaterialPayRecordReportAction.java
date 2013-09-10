12a706507614c324bc07142946994be9809a4a41
diego <diego.loza.click@gmail.com>
2013-09-10 11:16:15 -0400

    reporte de pago de productores

---
+++ src/main/com/encens/khipus/action/production/reports/rawMaterialPayRecordReportAction.java
@@ -0,0 +1,11 @@
+package com.encens.khipus.action.production.reports;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Diego
+ * Date: 9/09/13
+ * Time: 12:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class rawMaterialPayRecordReportAction {
+}
