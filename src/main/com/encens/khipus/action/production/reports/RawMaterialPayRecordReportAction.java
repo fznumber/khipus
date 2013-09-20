2faa9b776bad365b66c15ecc96da5ce3b37fd2b6
diego <diego.loza.click@gmail.com>
2013-09-20 09:25:07 -0400

    Nombre corregido

---
+++ src/main/com/encens/khipus/action/production/reports/RawMaterialPayRecordReportAction.java
@@ -0,0 +1,11 @@
+package com.encens.khipus.action.production.reports;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Diego
+ * Date: 20/09/13
+ * Time: 9:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class RawMaterialPayRecordReportAction {
+}
