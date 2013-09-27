e53c03f77a4112eb16ecdf69d4b6b3799137b2c6
diego <diego.loza.click@gmail.com>
2013-09-27 10:25:13 -0400

    unir con Ariel

---
+++ src/main/com/encens/khipus/model/production/TypeMovementProducer.java
@@ -0,0 +1,11 @@
+package com.encens.khipus.model.production;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Diego
+ * Date: 26/09/13
+ * Time: 20:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class TypeMovementProducer {
+}
