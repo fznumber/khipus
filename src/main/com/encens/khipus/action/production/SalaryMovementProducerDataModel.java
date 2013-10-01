0e6c569809ec8e329151509ce855d700b7bd4dbf
diego <diego.loza.click@gmail.com>
2013-10-01 11:56:32 -0400

    + filtro

--- src/main/com/encens/khipus/action/production/SalaryMovementProducerDataModel.java
+++ src/main/com/encens/khipus/action/production/SalaryMovementProducerDataModel.java
@@ -10,6 +10,7 @@ import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -24,10 +25,13 @@ import java.util.List;
 public class SalaryMovementProducerDataModel extends QueryDataModel<Long, SalaryMovementProducer> {
 
     private static final String[] RESTRICTIONS = {
-            "lower(salaryMovementProducer.rawMaterialProducer.firstName) like concat('%',concat(#{salaryMovementProducerDataModel.criteria.rawMaterialProducer.name}, '%'))",
-            "lower(salaryMovementProducer.rawMaterialProducer.lastName) like concat(#{salaryMovementProducerDataModel.criteria.rawMaterialProducer.lastName}, '%')",
-            "lower(salaryMovementProducer.rawMaterialProducer.maidenName) like concat(#{salaryMovementProducerDataModel.criteria.rawMaterialProducer.maidenName}, '%')"
+            //"lower(salaryMovementProducer.description) like concat('%',concat(#{salaryMovementProducerDataModel.criteria.description}, '%'))"
+            "rawMaterialProducer.firstName like concat(#{salaryMovementProducerDataModel.privateCriteria.rawMaterialProducer.firstName}, '%')"//,
+            /*"lower(salaryMovementProducer.rawMaterialProducer.lastName) like concat(#{salaryMovementProducerDataModel.criteria.rawMaterialProducer.lastName}, '%')",
+            "lower(salaryMovementProducer.rawMaterialProducer.maidenName) like concat(#{salaryMovementProducerDataModel.criteria.rawMaterialProducer.maidenName}, '%')"*/
     };
+
+    private PrivateCriteria privateCriteria;
 
     @Create
     public void init() {
@@ -38,12 +42,32 @@ public class SalaryMovementProducerDataModel extends QueryDataModel<Long, Salary
     public String getEjbql() {
         String query = " select salaryMovementProducer " +
                        " from SalaryMovementProducer salaryMovementProducer " +
-                       " join salaryMovementProducer.rawMaterialProducer ";
+                       " left join salaryMovementProducer.rawMaterialProducer rawMaterialProducer";
         return query;
     }
 
     @Override
     public List<String> getRestrictions() {
         return Arrays.asList(RESTRICTIONS);
+    }
+
+    public PrivateCriteria getPrivateCriteria() {
+        if (privateCriteria == null) {
+            privateCriteria = new PrivateCriteria();
+        }
+        return privateCriteria;
+    }
+
+    public static class PrivateCriteria {
+
+        private RawMaterialProducer rawMaterialProducer = new RawMaterialProducer();
+
+        public RawMaterialProducer getRawMaterialProducer() {
+            return rawMaterialProducer;
+        }
+
+        public void setRawMaterialProducer(RawMaterialProducer rawMaterialProducer) {
+            this.rawMaterialProducer = rawMaterialProducer;
+        }
     }
 }
