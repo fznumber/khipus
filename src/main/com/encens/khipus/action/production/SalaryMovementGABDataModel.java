b1cec30b5fecc1e3c859388bfcfe445bccf3c511
diego <diego.loza.click@gmail.com>
2013-10-01 18:30:43 -0400

    filtros movimiento de salarios gab

--- src/main/com/encens/khipus/action/production/SalaryMovementGABDataModel.java
+++ src/main/com/encens/khipus/action/production/SalaryMovementGABDataModel.java
@@ -25,12 +25,10 @@ import java.util.List;
 public class SalaryMovementGABDataModel extends QueryDataModel<Long, SalaryMovementGAB> {
 
     private static final String[] RESTRICTIONS = {
-            "lower(productiveZone.number) like concat(#{salaryMovementGABDataModel.privateCriteria.productiveZone.number}, '%')",
-            "lower(productiveZone.group) like concat(#{salaryMovementGABDataModel.privateCriteria.productiveZone.group}, '%')",
-            "lower(productiveZone.name) like concat(#{salaryMovementGABDataModel.privateCriteria.productiveZone.name}, '%')",
+            "productiveZone.number like concat(#{salaryMovementGABDataModel.criteria.productiveZone.number}, '%')",
+            "productiveZone.group like concat(#{salaryMovementGABDataModel.criteria.productiveZone.group}, '%')",
+            "productiveZone.name like concat(#{salaryMovementGABDataModel.criteria.productiveZone.name}, '%')",
     };
-
-    private PrivateCriteria privateCriteria;
 
     @Create
     public void init() {
@@ -41,8 +39,15 @@ public class SalaryMovementGABDataModel extends QueryDataModel<Long, SalaryMovem
     public String getEjbql() {
         String query = " select salaryMovementGAB " +
                        " from SalaryMovementGAB salaryMovementGAB " +
-                       " join salaryMovementGAB.productiveZone productiveZone";
+                       " left join fetch salaryMovementGAB.productiveZone productiveZone";
         return query;
+    }
+
+    @Override
+    public SalaryMovementGAB createInstance() {
+        SalaryMovementGAB salaryMovementGAB = super.createInstance();
+        salaryMovementGAB.setProductiveZone(new ProductiveZone());
+        return salaryMovementGAB;
     }
 
     @Override
@@ -50,23 +55,4 @@ public class SalaryMovementGABDataModel extends QueryDataModel<Long, SalaryMovem
         return Arrays.asList(RESTRICTIONS);
     }
 
-    public PrivateCriteria getPrivateCriteria() {
-        if (privateCriteria == null) {
-            privateCriteria = new PrivateCriteria();
-        }
-        return privateCriteria;
-    }
-
-    public static class PrivateCriteria {
-
-        private ProductiveZone productiveZone = new ProductiveZone();
-
-        public ProductiveZone getProductiveZone() {
-            return productiveZone;
-        }
-
-        public void setProductiveZone(ProductiveZone productiveZone) {
-            this.productiveZone = productiveZone;
-        }
-    }
 }
