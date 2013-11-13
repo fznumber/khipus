caa36f4b9b85f67f7e4af623cbf7b68ba2d92214
diego <diego.loza.click@gmail.com>
2013-11-13 12:20:25 -0400

    Fixed bugs

---
+++ src/main/com/encens/khipus/model/production/ArticleEstate.java
@@ -0,0 +1,11 @@
+package com.encens.khipus.model.production;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Diego
+ * Date: 12/11/13
+ * Time: 16:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class ArticleEstate {
+}
---
+++ src/main/com/encens/khipus/service/production/ArticleEstateServiceBean.java
@@ -0,0 +1,11 @@
+package com.encens.khipus.service.production;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Diego
+ * Date: 12/11/13
+ * Time: 19:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class ArticleEstateServiceBean {
+}
