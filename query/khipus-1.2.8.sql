bc14301c234400c756f9a92de59ed2eef740745f
diego <diego.loza.click@gmail.com>
2013-12-27 18:56:22 -0400

    version

--- query/khipus-1.2.8.sql
+++ query/khipus-1.2.8.sql
@@ -3,4 +3,5 @@
 --Fecha de creacion: 27/12/2013
 alter table costosindirectosconf  add  (ESTADO VARCHAR(255) NULL);
 alter table ordenproduccion drop constraint UQ_ORDENPRODUCCIONFECHA;
+drop index UQ_ORDENPRODUCCIONFECHA;
 --COMMIT
