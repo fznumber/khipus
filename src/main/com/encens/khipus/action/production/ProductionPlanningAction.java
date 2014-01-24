5765480ac7da08cadca68bb4412f797d794d50a3
diego <diego.loza.click@gmail.com>
2014-01-24 10:09:24 -0400

    -cambio de version

--- src/main/com/encens/khipus/action/production/ProductionPlanningAction.java
+++ src/main/com/encens/khipus/action/production/ProductionPlanningAction.java
@@ -696,9 +696,9 @@ public class ProductionPlanningAction extends GenericAction<ProductionPlanning> 
 
     public String generateVoucherOrderProduction(){
 
-        //productionOrder.setEstateOrder(TABULATED);
+        productionOrder.setEstateOrder(TABULATED);
 
-        productionOrder.setEstateOrder(INSTOCK);
+        //productionOrder.setEstateOrder(INSTOCK);
 
         InventoryMovement inventoryMovement = createVale();
         approvalVoucher(inventoryMovement);
