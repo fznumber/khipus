8f5a259268fa5a3c617a7d86ec4c27696ba36860
diego <diego.loza.click@gmail.com>
2014-06-06 17:44:50 -0400

    OrderReceiptReportAction cambio en el nombre

--- src/main/com/encens/khipus/action/customers/reports/orderReceiptReportAction.java
+++ src/main/com/encens/khipus/action/customers/reports/orderReceiptReportAction.java
@@ -45,6 +45,7 @@ public class OrderReceiptReportAction extends GenericReportAction {
     private int postXIniAmount,widthIniAmount;
     private Date dateOrder;
     private Employee distributor;
+
     @In
     private AccountItemService accountItemService;
 
